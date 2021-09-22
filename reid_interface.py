4be24afc78c23d91fced7204a61599f500fb88aa
Zz-ww <664843519@qq.com>
2021-09-22 11:02:36 +0800

    modify the reid_interface

--- reid_interface.py
+++ reid_interface.py
@@ -1,21 +1,23 @@
 from reiddata.models import *
-import  reiddata.defaults as reidCfg
+import reiddata.defaults as reidCfg
 from reiddata.reid_init import *
 from reiddata.utils import *
 
+
 class ReID:
-    def __init__(self, yolo_model_path,yolo_model_cfg,yolo_data_path,reid_model_path):
+    def __init__(self, yolo_model_path, yolo_model_cfg, yolo_data_path, reid_model_path):
+
+        self.device = select_device(force_cpu=False)
+        self.query_loader, self.data_query = make_data_loader(reidCfg._C)
+        self.query_feats = []
+        self.reidModel = build_model(reidCfg._C, reid_model_path, self.device)
         with torch.no_grad():
-            self.device =select_device(force_cpu=False)
-            self.query_loader, self.data_query = make_data_loader(reidCfg._C)
-            self.reidModel = build_model(reidCfg._C, reid_model_path, self.device)
-            self.query_feats= []
-            self.query_feats = make_query(self.query_loader, self.device, self.reidModel, self.query_feats)
-            self.model=Darknet(yolo_model_cfg, 416)
+            self.model = Darknet(yolo_model_cfg, 416)
             _ = load_darknet_weights(self.model, yolo_model_path)
             self.model.to(self.device).eval()
-            self.classes = load_classes(yolo_data_path)
-            self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.classes))]
+        self.query_feats = make_query(self.query_loader, self.device, self.reidModel, self.query_feats)
+        self.classes = load_classes(yolo_data_path)
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.classes))]
 
     def data_process(self, im0):
         im0 = cv2.cvtColor(im0, cv2.COLOR_BGR2RGB)
@@ -24,11 +26,12 @@ class ReID:
         img = np.ascontiguousarray(img, dtype=np.float32)
         img /= 255.0
         img = torch.from_numpy(img).unsqueeze(0).to(self.device)
-        return img,im0
+        return img, im0
 
-    def __call__(self, im0,video_writer,all_id,debug=False):
-        img, im0=self.data_process(im0)
-        pred, _ = self.model(img)
+    def __call__(self, im0, all_id, video_writer=None, debug=False):
+        img, im0 = self.data_process(im0)
+        with torch.no_grad():
+            pred, _ = self.model(img)
         det = non_max_suppression(pred.float(), 0.1, 0.45)[0]
         if det is not None and len(det) > 0:
             det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
@@ -37,7 +40,7 @@ class ReID:
             pic_list = []
             for *xyxy, conf, cls_conf, cls in det:
                 if self.classes[int(cls)] == 'person':
-                    xmin,ymin,xmax ,ymax= int(xyxy[0]), int(xyxy[1]),int(xyxy[2]),int(xyxy[3])
+                    xmin, ymin, xmax, ymax = int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])
                     w = xmax - xmin
                     h = ymax - ymin
                     if w * h > 500:
@@ -49,7 +52,8 @@ class ReID:
             if gallery_img:
                 gallery_img = torch.cat(gallery_img, dim=0)
                 gallery_img = gallery_img.to(self.device)
-                gallery_feats = self.reidModel(gallery_img)
+                with torch.no_grad():
+                    gallery_feats = self.reidModel(gallery_img)
                 gallery_feats = torch.nn.functional.normalize(gallery_feats, dim=1, p=2)
                 m, n = self.query_feats.shape[0], gallery_feats.shape[0]
                 distmat = torch.pow(self.query_feats, 2).sum(dim=1, keepdim=True).expand(m, n) + \
@@ -61,7 +65,7 @@ class ReID:
                     if distmat_min_value[k] < 1.0:
                         index = int(np.argwhere(distmat[:, k] == distmat_min_value[k])[0])
                         pic_list.append([k, index, distmat_min_value[k]])
-                        name=self.data_query[index][0].split('/')[-1].split('_')[0]
+                        name = self.data_query[index][0].split('/')[-1].split('_')[0]
                         if name not in all_id:
                             all_id.append(name)
                 if debug:
@@ -70,7 +74,3 @@ class ReID:
                         plot_one_box(gallery_loc[draw_pic[0]], im0, label=name, color=self.colors[int(cls)])
         if debug:
             video_writer.write(im0)
-        return all_id
-
-
-
